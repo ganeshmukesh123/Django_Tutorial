2e61a41d941cc03dd30105dac00fa4e27715209a
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-07-15 10:10:43 +0530

    creating template

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
 from . import views
 urlpatterns = [
-    url(r'^$',views.index, name='index' ),
-    url(r'^contact/$', views.contact, name='contact')
+    url(r'^$',ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name='blog/blog.html' ))
 ]
