ec5e394b5a61c92b6324f1ff97b62e4a58e05e05
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-07-23 09:49:33 +0530

    Adding all files in newfolder "Simple_blog"
    In this blog all blog are wriiten by admin only

--- Simple_blog/mysite/settings.py
+++ Simple_blog/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog',
     'personal',
+    'django_numpy',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
